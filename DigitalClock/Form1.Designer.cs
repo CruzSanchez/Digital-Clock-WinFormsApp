3052bd5be6558123e57f177e586fc7d5ce35ba36
CruzSanchez <Cruzsanchez9209@gmail.com>
2018-08-30 12:08:02 -0500

    Clock is working, repeating self in code, REFACTOR!

--- DigitalClock/Form1.Designer.cs
+++ DigitalClock/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DigitalClock
 {
-	partial class Form1
+	partial class Clock
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -28,13 +28,42 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.BackColor = System.Drawing.Color.Black;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.Lime;
+			this.label1.Location = new System.Drawing.Point(62, 79);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(679, 160);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "00:00:00";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// Clock
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(800, 337);
+			this.Controls.Add(this.label1);
+			this.Name = "Clock";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Digital Clock";
+			this.Load += new System.EventHandler(this.Clock_Load);
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label label1;
 	}
 }
 
--- DigitalClock/Form1.cs
+++ DigitalClock/Form1.cs
@@ -10,11 +10,63 @@ using System.Windows.Forms;
 
 namespace DigitalClock
 {
-	public partial class Form1 : Form
+	public partial class Clock : Form
 	{
-		public Form1()
+		Timer timer = new Timer();
+		public Clock()
 		{
 			InitializeComponent();
+		}
+
+		private void Clock_Load(object sender, EventArgs e)
+		{
+			timer.Interval = 1000;
+
+			timer.Tick += new EventHandler(this.timer_Tick);
+
+			timer.Start();
+		}
+
+		private void timer_Tick(object sender, EventArgs e)
+		{
+			int hh = DateTime.Now.Hour;
+			int mm = DateTime.Now.Minute;
+			int ss = DateTime.Now.Second;
+
+			string time = "";
+
+			if(hh < 10)
+			{
+				time += "0" + hh;
+			}
+			else
+			{
+				time += hh;
+			}
+
+			time += ":";
+
+			if (mm < 10)
+			{
+				time += "0" + mm;
+			}
+			else
+			{
+				time += mm;
+			}
+
+			time += ":";
+
+			if (ss < 10)
+			{
+				time += "0" + ss;
+			}
+			else
+			{
+				time += ss;
+			}
+
+			label1.Text = time;
 		}
 	}
 }
